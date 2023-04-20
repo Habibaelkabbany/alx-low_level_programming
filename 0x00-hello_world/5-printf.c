4605ce013ab6e2465d547baf4bd2396f44269c98
Habibaelkabbany <helkabbany2@gmail.com>
2023-04-20 16:45:51 -0700

    Fifth task

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main -  prints required, followed by a new line using puts().
+ * Return: integer Zero
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
