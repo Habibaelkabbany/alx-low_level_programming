6f320ff9ac695f0759286eec9f1723dd60aced29
Habibaelkabbany <helkabbany2@gmail.com>
2023-04-20 16:31:36 -0700

    Fourth

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main -  prints required, followed by a new line using puts().
+ * Return: integer Zero
+ */
+int main(void)
+{
+puts("Programming is like building a multilingual puzzle");
+return (0);
+}
