b34bd4a95be22cbebc7290f20707a6aa83551aa7
Habibaelkabbany <helkabbany2@gmail.com>
2023-04-21 17:06:40 -0700

    4th

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main -  prints required, followed by a new line using puts().
+ * Return: integer Zero
+ */
+int main(void)
+{
+int n;
+n = 'a';
+while (n <= 'z')
+{
+	putchar(n);
+	n++;
+}
+n = 'A';
+while (n <= 'Z')
+{
+	putchar(n);
+	n++;
+}
+putchar('\n');
+return (0);
+}
