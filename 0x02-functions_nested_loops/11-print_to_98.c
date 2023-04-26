459012447dec26750c9c8a57884e5770b945d506
Habibaelkabbany <helkabbany2@gmail.com>
2023-04-26 16:47:23 -0700

    dts

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,57 +7,19 @@
 
 void print_to_98(int n)
 {
-int temp;
-
 if (n < 98)
 {
 	for ( ; n < 98; n++)
 	{
-		if (n < 0)
-		{
-			_putchar('-');
-			temp = n * -1;
-		}
-		else
-		{
-			temp = n;
-		}
-		if (temp < 10)
-		{
-			_putchar('0' + temp);
-		}
-		else
-		{
-			_putchar('0' + temp / 10);
-			_putchar('0' + temp % 10);
-		}
-		_putchar(',');
-		_putchar(' ');
+		printf("%d, ", n);
 	}
-	_putchar('9');
-	_putchar('8');
-	_putchar('\n');
 }
 else
 {
 	for ( ; n > 98; n--)
 	{
-		if (n < 100)
-		{
-			_putchar('0' + n / 10);
-			_putchar('0' + n % 10);
-		}
-		else
-		{
-			_putchar('0' + n / 100);
-			_putchar('0' + (n / 10) % 10);
-			_putchar('0' + n % 10);
-		}
-		_putchar(',');
-		_putchar(' ');
+		printf("%d, ", n);
 	}
-	_putchar('9');
-	_putchar('8');
-	_putchar('\n');
 }
+printf("98\n");
 }
