ec7f8fdf05724827feb37fc68f2b9af0d2841081
Habibaelkabbany <helkabbany2@gmail.com>
2023-04-26 18:05:32 -0700

    dts33

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -9,16 +9,18 @@
 int main(void)
 {
 int x;
-
+int sum;
+sum = 0;
 x = 1;
 while (x < 1024)
 {
 	if (x % 3 == 0 || x % 5 == 0)
 	{
-		printf("%d\n", x);
+		sum += x;
 	}
 	x++;
 
 }
+printf("%d\n", sum);
 return (0);
 }
