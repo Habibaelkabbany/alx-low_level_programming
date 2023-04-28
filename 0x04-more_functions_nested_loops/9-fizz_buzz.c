b2be5005ab25c82a55a112ac4194c4398d53f3ca
Habibaelkabbany <helkabbany2@gmail.com>
2023-04-27 17:55:52 -0700

    9th

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,7 +9,7 @@
 int main(void)
 {
 int i;
-for (i = 1; i <= 100; i++)
+for (i = 1; i < 100; i++)
 {
 	if (i % 3 == 0)
 	{
@@ -25,6 +25,7 @@ for (i = 1; i <= 100; i++)
 		printf("%d ", i);
 	}
 }
+printf("Buzz");
 printf("\n");
 return (0);
 }
