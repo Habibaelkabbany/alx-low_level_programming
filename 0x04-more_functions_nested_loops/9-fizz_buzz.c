06ca52ef51e409361747eaff20f7622f66aa7dd5
Habibaelkabbany <helkabbany2@gmail.com>
2023-04-27 17:43:17 -0700

    10th

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *main - fizzing and buzzing
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+int i;
+for (i = 1; i <= 100; i++)
+{
+	if (i % 3 == 0)
+	{
+		printf("Fizz");
+	}
+	if (i % 5 == 0)
+	{
+		printf("Buzz");
+	}
+	printf(" ");
+	if (i % 3 != 0 && i % 5 != 0)
+	{
+		printf("%d ", i);
+	}
+}
+return (0);
+}
