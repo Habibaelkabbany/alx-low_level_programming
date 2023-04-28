2f7aa8733bdebead5123b09a27060070d82cfa16
Habibaelkabbany <helkabbany2@gmail.com>
2023-04-28 04:32:09 -0700

    3 & 4

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "main.h"
 /**
- *print_numbers - prints numbers from 0 to 0 ex 2 & 4
+ *print_numbers - prints numbers from 0 to 9
  */
 
 void print_numbers(void)
@@ -9,10 +9,7 @@ void print_numbers(void)
 int i;
 for (i = '0'; i <= '9'; i++)
 {
-	if (i != '2' && i != '4')
-	{
-		_putchar(i);
-	}
+_putchar(i);
 }
 _putchar('\n');
 }
