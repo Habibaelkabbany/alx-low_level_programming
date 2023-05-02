be1c7b5a974f022ebf5234c1339e1b1316803833
Habibaelkabbany <helkabbany2@gmail.com>
2023-05-02 13:46:43 -0700

    1st

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -5,14 +5,14 @@
 
 
 void reset_to_98(int *n);
-int _isdigit(int c);
-int mul(int a, int b);
-void print_most_numbers(void);
-void more_numbers(void);
-void print_line(int n);
-void print_diagonal(int n);
-void print_square(int size);
-void print_triangle(int size);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
 int _putchar(char c);
 void print_numbers(void);
 void print_number(int n);
