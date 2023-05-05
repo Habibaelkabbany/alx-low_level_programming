c3b30dd56745579cb29d7d4a10aef8596fa6b9d6
Habibaelkabbany <helkabbany2@gmail.com>
2023-05-05 03:21:25 -0700

    try

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,26 +9,19 @@
 int main(void)
 {
 long int n;
+int c;
 long int x;
 long int r;
 x = 2;
 n = 1;
 printf("%ld, %ld", n, x);
-while (r <= 4000000)
-{
-
-if (r > 4000000)
-{
-break;
-}
-if (r % 2 == 0)
+for (c = 2; c < 96; c++)
 {
 r = n + x;
-}
+printf(", %ld", r);
 n = x;
 x = r;
 }
-printf(", %ld", r);
 printf("\n");
 return (0);
 }
