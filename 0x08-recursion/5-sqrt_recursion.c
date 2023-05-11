c77b4a7b5c0b0bc051bb6ae2f13304f1a1d0b361
Habibaelkabbany <helkabbany2@gmail.com>
2023-05-11 15:39:33 -0700

    5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _sqrt_recursion - gets a number to the root of another
+ *@n: base number
+ *
+ * Return: resultant root
+ */
+
+int _sqrt_recursion(int n)
+{
+return (_sqrt_helper(n, n - 1));
+}
+/**
+ * _sqrt_helper - gets a number to the root of another
+ *@n: base number
+ *@y: divisor in question
+ *
+ * Return: resultant root
+ */
+int _sqrt_helper(int n, int y)
+{
+if (y <= 0)
+{
+	return (-1);
+}
+else if ((y * y) == n)
+{
+	return (y);
+}
+else
+{
+	return (_sqrt_helper(n, y - 1));
+}
+}
