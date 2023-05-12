86be81edd26f2df0395ecc0c0ffa3ce6f38c305b
Habibaelkabbany <helkabbany2@gmail.com>
2023-05-12 15:32:34 -0700

    1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of the program
+ *@argc: count of argmunets
+ *@argv: array of arguments
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char __attribute__ ((unused))*argv[])
+{
+
+printf("%d\n", argc - 1);
+
+
+return (0);
+}
