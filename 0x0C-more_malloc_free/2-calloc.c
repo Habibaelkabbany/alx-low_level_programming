563c8ff4461ab3750ef18ef0e734b8e916137805
Habibaelkabbany <helkabbany2@gmail.com>
2023-05-21 04:20:14 -0700

    2.2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -13,6 +13,7 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 char *s;
+unsigned int i;
 
 if (nmemb == 0 || size == 0)
 {
@@ -22,6 +23,10 @@ s = malloc(nmemb * size);
 if (s == NULL)
 {
 	return (NULL);
+}
+for (i = 0; i < (nmemb * size); i++)
+{
+	s[i] = 0;
 }
 return (s);
 }
