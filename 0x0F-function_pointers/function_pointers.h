95e19d236b4b4facbccc04baa9e668ce0934d9d2
Habibaelkabbany <helkabbany2@gmail.com>
2023-05-24 16:46:03 -0700

    0

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,20 @@
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+
+void print_name(char *name, void (*f)(char *));
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+void print_chessboard(char (*a)[8]);
+void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
+unsigned int count(char *s1);
+
+#endif
