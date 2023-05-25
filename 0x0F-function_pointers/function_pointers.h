bff40da0e2847b6036b7842ec910c463a35b85dd
Habibaelkabbany <helkabbany2@gmail.com>
2023-05-24 17:00:44 -0700

    1

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -6,7 +6,7 @@
 
 
 void print_name(char *name, void (*f)(char *));
-char *string_nconcat(char *s1, char *s2, unsigned int n);
+void array_iterator(int *array, size_t size, void (*action)(int));
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
