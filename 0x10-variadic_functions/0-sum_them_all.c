7744fc73307158dc8e9f651c5af6f6a0854e0185
Habibaelkabbany <helkabbany2@gmail.com>
2023-05-27 18:12:00 -0700

    0

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - sums all ints
+ * @n: number of args
+ * @...: ellips to signify numerous args
+ *
+ * ieturn: sum of numerous numbers.
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+va_list args;
+unsigned int sum;
+unsigned int i;
+
+va_start(args, n);
+sum = 0;
+i = 0;
+if (n == 0)
+	return (0);
+while (i < n)
+{
+	sum += va_arg(args, int);
+	i++;
+}
+va_end(args);
+return (sum);
+}
---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,28 @@
+#ifndef CALC_H
+#define CALC_H
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * struct op - Struct op
+ *
+ * @op: The operator
+ * @f: The function associated
+ */
+typedef struct op
+{
+	char *op;
+	int (*f)(int a, int b);
+} op_t;
+int sum_them_all(const unsigned int n, ...);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+int (*get_op_func(char *s))(int, int);
+void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
+unsigned int count(char *s1);
+
+#endif
