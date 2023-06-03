6568510307dcf0c0143ea9bec778351fbd37a800
Habibaelkabbany <helkabbany2@gmail.com>
2023-06-02 17:34:10 -0700

    100

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ * before_main - check the code
+ *
+ * Return: Always 0.
+ */
+void before_main(void) __attribute__((constructor));
+
+void before_main(void)
+{
+printf
+	("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
