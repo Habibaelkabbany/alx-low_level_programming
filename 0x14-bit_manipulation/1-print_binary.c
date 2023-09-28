ce5608d14155d4ae2ce9f5eaf0f3146db06587eb
Habiba Elkabbany <helkabbany2@gmail.com>
2023-09-28 15:52:40 -0700

    attempt 2

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,19 +9,8 @@
  */
 void print_binary(unsigned long int n)
 {
-	static int printed;
-
-	if (n == 0)
-	{
-		if (!printed)
-			printf("0");
-		printf("\n");
-		return;
-	}
-	else
-	{
-		printed = 1;
+	if (n > 1)
 		print_binary(n >> 1);
-		printf("%lu", (n & 1));
-	}
+
+	putchar((n & 1) + '0');
 }
