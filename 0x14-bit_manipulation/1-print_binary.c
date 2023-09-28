48c6a0be2287ee078e7e681379117629a2361190
Habiba Elkabbany <helkabbany2@gmail.com>
2023-09-28 15:32:56 -0700

    Lets hope

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdlib.h>
+#include <limits.h>
+#include <stdio.h>
+
+/**
+ * print_binary - print binary rep
+ * @n: decimal
+ */
+void print_binary(unsigned long int n)
+{
+	static int printed;
+
+	if (n == 0)
+	{
+		if (!printed)
+			printf("0");
+		return;
+	}
+	else
+	{
+		printed = 1;
+		print_binary(n >> 1);
+		printf("%lu", (n & 1));
+	}
+}
